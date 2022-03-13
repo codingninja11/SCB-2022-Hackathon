315ce1d1fc045a41d6023dbe610ec15832b47415
codingninja11 <akritisheresth@gmail.com>
2022-03-13 08:53:59 +0530

    finalising

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
