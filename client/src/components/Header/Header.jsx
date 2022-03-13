fcb41b84580045e6a39c1c9d8e6a9c0e845537a7
codingninja11 <akritisheresth@gmail.com>
2022-03-13 14:38:59 +0530

    responsive-header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,26 +1,23 @@
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Nav, Navbar } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
-import "./Header.css";
 const Header = () => {
-  const user = localStorage.getItem("userName");
+  const user = localStorage.getItem('userName');
   const handleClick = () => {
     localStorage.clear();
-    window.location.href = "/";
+    window.location.href = '/';
   };
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "space-around",
-        alignItems: "center",
-        fontSize: "1.5rem",
-        background: "#0F1624",
-        color: "#fff",
-        height: "10vh",
-        textDecoration: "none",
-        pointer: "cursor",
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        fontSize: '1.5rem',
+        background: '#0F1624',
+        color: '#fff',
+        height: '10vh',
+        textDecoration: 'none',
+        pointer: 'cursor',
       }}
     >
       <Link to="/">
@@ -30,63 +27,49 @@ const Header = () => {
           </h3>
         </span>
       </Link>
-      <div className="Header">
-        <Navbar
-          bg="#0F1624"
-          variant="dark"
-          sticky="top"
-          expand="sm"
-          collapseOnSelect
-        >
-          <Navbar.Toggle className="coloring" />
-          <Navbar.Collapse>
-            <Nav>
-              <div style={{ display: "flex" }}>
-                <div style={{ marginRight: "3rem" }}>
-                  <Link to="/">Home</Link>
-                </div>
 
-                <div style={{ marginRight: "3rem" }}>
-                  <Link to="/about">About</Link>
-                </div>
-                {user ? (
-                  <>
-                    <div
-                      style={{
-                        marginRight: "3rem",
-                      }}
-                    >{`Hello "${user}"`}</div>
-                    <div
-                      style={{
-                        marginRight: "3rem",
-                      }}
-                      onClick={handleClick}
-                    >
-                      Logout
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div
-                      style={{
-                        marginRight: "3rem",
-                      }}
-                    >
-                      <Link to="/login">Login</Link>
-                    </div>
-                    <div
-                      style={{
-                        marginRight: "3rem",
-                      }}
-                    >
-                      <Link to="/register">Register</Link>
-                    </div>
-                  </>
-                )}
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+      <div style={{ display: 'flex' }}>
+        <div style={{ marginRight: '3rem' }}>
+          <Link to="/">Home</Link>
+        </div>
+
+        <div style={{ marginRight: '3rem' }}>
+          <Link to="/about">About</Link>
+        </div>
+        {user ? (
+          <>
+            <div
+              style={{
+                marginRight: '3rem',
+              }}
+            >{`Hello "${user}"`}</div>
+            <div
+              style={{
+                marginRight: '3rem',
+              }}
+              onClick={handleClick}
+            >
+              Logout
+            </div>
+          </>
+        ) : (
+          <>
+            <div
+              style={{
+                marginRight: '3rem',
+              }}
+            >
+              <Link to="/login">Login</Link>
+            </div>
+            <div
+              style={{
+                marginRight: '3rem',
+              }}
+            >
+              <Link to="/register">Register</Link>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
